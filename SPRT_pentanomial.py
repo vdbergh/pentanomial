9da78f581bf7a9ebcf42e80544dbee90371292a1
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2020-02-21 20:13:58 +0100

    Bring things up to date with the Fishtest GSPRT implementation.
    Note that this Python simulator is extremely slow. The multi-threaded
    C version
    
    https://github.com/vdbergh/simul
    
    is much much faster.

--- SPRT_pentanomial.py
+++ SPRT_pentanomial.py
@@ -27,18 +27,34 @@ class SPRT:
         self.LA=math.log(beta/(1-alpha))
         self.LB=math.log((1-beta)/alpha)
         self.status_=''
-        self.LLR_=0
+        self.LLR_=0.0
+        self.min_LLR=0.0
+        self.max_LLR=0.0
+        self.sq0=0.0
+        self.sq1=0.0
+        self.o0=0.0
+        self.o1=0.0
 
     def record(self,result):
         if self.status_!='':
             return
         self.results_[result]+=1
-        self.LLR_,overshoot=LLRcalc.LLR_logistic(self.elo0,self.elo1,self.results_)
-        # sanitize
-        overshoot=min((self.LB-self.LA)/20,overshoot)
-        if self.LLR_>self.LB-overshoot:
+        self.LLR_=LLRcalc.LLR_logistic(self.elo0,self.elo1,self.results_)
+
+        # Dynamic overshoot correction using
+        # Siegmund - Sequential Analysis - Corollary 8.33.
+        if(self.LLR_>self.max_LLR):
+            self.sq1+=(self.LLR_-self.max_LLR)**2
+            self.max_LLR=self.LLR_
+            self.o1=self.sq1/self.LLR_/2
+        if self.LLR_<self.min_LLR:
+            self.sq0+=(self.LLR_-self.min_LLR)**2
+            self.min_LLR=self.LLR_
+            self.o0=-self.sq0/self.LLR_/2
+
+        if self.LLR_>self.LB-self.o1:
             self.status_='H1'
-        elif self.LLR_ < self.LA+overshoot:
+        elif self.LLR_ < self.LA+self.o0:
             self.status_='H0'
 
     def status(self):
